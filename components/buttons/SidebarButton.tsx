1762d2c5ede94cbfff39dfdbae5c8bfc02f9513f
kschneider84 <77678452+kschneider84@users.noreply.github.com>
2023-03-01 11:56:59 +0100

    change button tag, add option of placing icon on right side

--- components/buttons/SidebarButton.tsx
+++ components/buttons/SidebarButton.tsx
@@ -3,22 +3,30 @@ import clsx from 'clsx';
 
 interface SidebarButtonProps extends ButtonHTMLAttributes<HTMLElement> {
     className?: string;
-    icon?: any;
+    Icon: React.ElementType;
+    iconOnRightSide?: boolean;
     label: string;
 }
 
-const SidebarButton: React.FC<SidebarButtonProps> = ({ icon, label, ...props }) => {
+const SidebarButton: React.FC<SidebarButtonProps> = ({ Icon, iconOnRightSide = false, label, ...props }) => {
     return (
-        <button
+        <a href={'./' + label}
             {...props}
             className={clsx(
                 'px-2 py-2 flex',
                 props.className,
             )}
         >
-            {icon}
-            {label}
-        </button>
+            {iconOnRightSide ? (
+                <>
+                    {label} &nbsp;  {Icon && <Icon className='h-6 w-6 mr-2 text-gray-400' />}
+                </>
+            ) : (
+                <>
+                    {Icon && <Icon className='h-6 w-6 mr-2 text-gray-400' />} {label}
+                </>
+            )}
+        </a>
     );
 };
 
