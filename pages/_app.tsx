3206ec8540e0f6ba985d3d268a5f477f66ddfe30
phev8 <phev8@users.noreply.github.com>
2023-04-03 11:29:30 +0200

    use default head component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,14 +5,18 @@ import "@fontsource/open-sans/600.css";
 import "@fontsource/open-sans/700.css";
 import type { AppProps } from 'next/app'
 import { SessionProvider } from "next-auth/react"
-import CustomHead from '@/local-components/CustomHead';
+import DefaultHead from '@/components/DefaultHead';
+
 
 export default function App({ Component, pageProps: {
     session,
     ...pageProps
 } }: AppProps) {
     return <SessionProvider session={session}>
-        <CustomHead />
+        <DefaultHead
+            title={process.env.NEXT_PUBLIC_APP_NAME || ''}
+            description='This is the CASE admin tool, to manage studies, surveys, messages and participants.'
+        />
         <Component {...pageProps} />
     </SessionProvider>
 }
