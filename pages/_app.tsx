176839882d3a66102fd8abae42d8e436427a1f92
phev8 <phev8@users.noreply.github.com>
2023-04-03 11:39:10 +0200

    use next font

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,22 +1,28 @@
 import '@/styles/globals.css'
-import "@fontsource/open-sans/300.css";
-import "@fontsource/open-sans/400.css";
-import "@fontsource/open-sans/600.css";
-import "@fontsource/open-sans/700.css";
 import type { AppProps } from 'next/app'
 import { SessionProvider } from "next-auth/react"
 import DefaultHead from '@/components/DefaultHead';
+
+import { Open_Sans } from 'next/font/google'
+
+const open_sans = Open_Sans({
+    subsets: ['latin'],
+    variable: '--font-open-sans',
+})
 
 
 export default function App({ Component, pageProps: {
     session,
     ...pageProps
 } }: AppProps) {
-    return <SessionProvider session={session}>
+    return <SessionProvider
+        session={session}>
         <DefaultHead
             title={process.env.NEXT_PUBLIC_APP_NAME || ''}
             description='This is the CASE admin tool, to manage studies, surveys, messages and participants.'
         />
-        <Component {...pageProps} />
+        <main className={`${open_sans.variable} font-sans`}>
+            <Component {...pageProps} />
+        </main>
     </SessionProvider>
 }
