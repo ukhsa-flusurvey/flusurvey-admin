a7679c3eed647fe1b4e9bf72c0da8203cffb3602
phev8 <phev8@users.noreply.github.com>
2023-04-06 11:12:36 +0200

    add new study page file

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -52,7 +52,6 @@ const CASECredentialProvider = CredentialsProvider({
             console.error(error);
             throw error;
         }
-        return null;
     },
 })
 
---
+++ pages/studies/new-study.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const NewStudy: React.FC = () => {
+    return (
+        <p>new-study</p>
+    );
+};
+
+export default NewStudy;
