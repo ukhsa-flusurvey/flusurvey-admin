da0a7aca3870cd28cb9395150f37cbca92cbd265
phev8 <phev8@users.noreply.github.com>
2023-04-26 13:34:18 +0200

    new landing page

--- app/(default)/service-status/error.tsx
+++ app/(default)/service-status/error.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import Container from '@/components/Container';
 import { useEffect } from 'react';
 
 export default function Error({
@@ -15,18 +16,12 @@ export default function Error({
     }, [error]);
 
     return (
-        <div>
-            <h2 className='text-2xl font-bold text-red-500'>{'Something went wrong :-('}</h2>
-            <p className='text-red-500'> {error.message} </p>
-            <button
-                className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'
-                onClick={
-                    // Attempt to recover by trying to re-render the segment
-                    () => reset()
-                }
-            >
-                Try again
-            </button>
-        </div>
+        <Container>
+            <div className='p-8 bg-white rounded-2xl shadow-lg max-w-[500px] mx-auto'>
+                <h2 className='text-2xl font-bold text-red-500'>{'Something went wrong :-('}</h2>
+                <p className='text-red-500 mt-4'> {error.message} </p>
+
+            </div>
+        </Container>
     );
 }
