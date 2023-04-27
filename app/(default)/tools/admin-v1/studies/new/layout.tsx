b847c0e5d7bf5ded4410c5ccfbae11713a28e74f
phev8 <phev8@users.noreply.github.com>
2023-04-27 08:48:50 +0200

    new study page has same navbar

--- app/(default)/tools/admin-v1/studies/new/layout.tsx
+++ app/(default)/tools/admin-v1/studies/new/layout.tsx
@@ -1,18 +1,27 @@
 import React from "react"
 import Container from "@/components/Container"
+import Navbar from "@/components/admin-tool-v1/Navbar"
 
 export default function Layout({ children }: { children: React.ReactNode }) {
     return (
         <div className="h-full bg-slate-100">
-            <div className='h-52 bg-cyan-800 -mb-52'>
-            </div>
-            <Container>
-                <div className="border-t border-white/25"></div>
+            <Navbar
+                links={[{
+                    title: 'Home',
+                    href: '/tools/admin-v1'
+                },
+                {
+                    title: 'Studies',
+                },
+                {
+                    title: 'New',
+                    href: '/tools/admin-v1/studies/new'
+                }
+                ]}
+            />
+            <Container className="py-8">
+                {children}
             </Container>
-            <Container className="my-8">
-                <h2 className="text-3xl font-semibold text-white">Create new study</h2>
-            </Container>
-            {children}
         </div>
     )
 }
--- app/(default)/tools/admin-v1/studies/new/page.tsx
+++ app/(default)/tools/admin-v1/studies/new/page.tsx
@@ -1,10 +1,7 @@
-import Container from "@/components/Container";
 import NewStudy from "@/components/admin-tool-v1/NewStudy";
 
 export default async function Page() {
     return (
-        <Container className="mb-16">
-            <NewStudy />
-        </Container>
+        <NewStudy />
     )
 }
