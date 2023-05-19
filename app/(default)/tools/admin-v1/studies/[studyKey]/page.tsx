9ec5cc02beb4a64659b8a069d03b8e11e6f26d36
phev8 <phev8@users.noreply.github.com>
2023-05-19 15:27:51 +0200

    add sidebar for study management

--- app/(default)/tools/admin-v1/studies/[studyKey]/page.tsx
+++ app/(default)/tools/admin-v1/studies/[studyKey]/page.tsx
@@ -50,43 +50,7 @@ export default async function Page({ params: { studyKey } }: {
 
             </section>
 
-            <section className="bg-white rounded p-6 shadow-sm flex flex-col divide-y">
-                <h3 className="text-2xl font-bold mb-4">Study rules</h3>
-                <div className="flex gap-4 py-4 flex-col sm:flex-row">
-                    <div className="w-full sm:w-[400px]">
-                        <h3 className="text-xl font-bold">Default Rules</h3>
-                        <p className="text-gray-500 text-sm">
-                            To update the default study rules use the file picker and click the button to upload.
-                        </p>
-                    </div>
-                    <div className="sm:ps-6">
-                        <StudyRuleUploader
-                            studyKey={studyKey}
-                        />
-                        <NotImplemented className="mt-2">
-                            see rule history (with update times) | preview active rules
-                        </NotImplemented>
-                    </div>
-                </div>
-                <div className="flex gap-4 flex-col sm:flex-row pt-4 ">
-                    <div className="w-full sm:w-[400px]">
-                        <h3 className="text-xl font-bold">Custom rules</h3>
-                        <p className="text-gray-500 text-sm">
-                            Custom rules can be triggered here manually. Here you can select a custom rule file and trigger the execution of the rules.
-                        </p>
-                    </div>
-                    <div className="sm:ps-6">
-                        <NotImplemented>
-                            select custom rule file | trigger rule execution
-                        </NotImplemented>
 
-                        <NotImplemented className="mt-2">
-                            see progress of rule execution
-                        </NotImplemented>
-                    </div>
-                </div>
-
-            </section>
         </div>
     )
 }
