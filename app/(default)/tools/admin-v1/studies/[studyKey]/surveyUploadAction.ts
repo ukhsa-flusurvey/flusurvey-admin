c414a4467165c66bd5691a0d8e1015e089774bf2
phev8 <phev8@users.noreply.github.com>
2023-07-26 14:30:58 +0200

    server action used in survey upload

---
+++ app/(default)/tools/admin-v1/studies/[studyKey]/surveyUploadAction.ts
@@ -0,0 +1,29 @@
+'use server'
+
+import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+import { getCASEManagementAPIURL } from "@/utils/server/api";
+import { Study } from "@/utils/server/types/studyInfos";
+import { getServerSession } from "next-auth/next";
+import { Expression, Survey } from "survey-engine/data_types";
+
+
+export const uploadSurvey = async (studyKey: string, survey: Survey): Promise<Study> => {
+    const session = await getServerSession(authOptions);
+    if (!session || !session.accessToken) throw new Error('unauthenticated');
+
+    const url = getCASEManagementAPIURL(`/v1/study/${studyKey}/surveys`);
+    const r = await fetch(url.toString(), {
+        method: 'POST',
+        body: JSON.stringify({ survey: survey }),
+        headers: {
+            'Authorization': `Bearer ${session?.accessToken}`,
+            'Content-Type': 'application/json'
+        },
+        next: { revalidate: 0 }
+    });
+    if (r.status !== 200) {
+        console.log(await r.json());
+        throw new Error('upload failed');
+    }
+    return r.json();
+}
