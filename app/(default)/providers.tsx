8d3b1cdb8d6aedd27cec41cb4d891f541d340d84
phev8 <phev8@users.noreply.github.com>
2023-12-14 10:59:07 +0100

    include navigation for nextui provider

--- app/(default)/providers.tsx
+++ app/(default)/providers.tsx
@@ -2,11 +2,14 @@
 
 import AuthProvider from '@/components/AuthProvider'
 import { NextUIProvider } from '@nextui-org/react'
+import { useRouter } from 'next/navigation';
 
 export function Providers({ children }: { children: React.ReactNode }) {
+    const router = useRouter();
+
     return (
         <AuthProvider>
-            <NextUIProvider>
+            <NextUIProvider navigate={router.push}>
                 {children}
             </NextUIProvider>
         </AuthProvider>
