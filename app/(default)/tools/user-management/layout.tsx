01ca47fa8eb540bef886daffb623a871b5a45eb8
phev8 <phev8@users.noreply.github.com>
2024-02-22 21:31:14 +0100

    feat: shadcn/ui card opaque variant
    chore: setup routes for user-management

---
+++ app/(default)/tools/user-management/layout.tsx
@@ -0,0 +1,19 @@
+import UserManagementAppbarBase from "./UserManagementAppbarBase";
+
+export default function Layout({
+    children,
+}: {
+    children: React.ReactNode;
+}) {
+
+    const appName = process.env.NEXT_PUBLIC_APP_NAME || 'Editor Tools';
+
+    return (
+        <div className="flex flex-col h-screen">
+            <UserManagementAppbarBase />
+            <div className="bg-cover bg-center bg-[url(/images/tree-on-hill.png)] grow w-full">
+                {children}
+            </div>
+        </div>
+    )
+}
--- app/(default)/tools/user-management/page.tsx
+++ app/(default)/tools/user-management/page.tsx
@@ -1,6 +1,6 @@
 import NotImplemented from "@/components/NotImplemented";
-import UserManagementAppbarBase from "./UserManagementAppbarBase";
-import { Card, CardBody } from "@nextui-org/card";
+import { Card } from "@/components/ui/card";
+
 
 export const dynamic = 'force-dynamic';
 
@@ -8,24 +8,16 @@ export default async function Page() {
     const appName = process.env.NEXT_PUBLIC_APP_NAME || 'Editor Tools';
 
     return (
-        <div className="flex flex-col h-screen">
-            <UserManagementAppbarBase />
-            <main className="bg-cover bg-center bg-[url(/images/tree-on-hill.png)] grow w-full">
-                <div className="backdrop-blur-md bg-white/50 h-full w-full flex items-center justify-center">
-                    <Card
-                        className="bg-white/80"
-                        isBlurred={true}
-                    >
-                        <CardBody>
-                            <NotImplemented
-                            >
-                                Methods for user management are not implemented yet
-                            </NotImplemented>
-                        </CardBody>
-                    </Card>
-                </div>
-            </main>
+        <main className="h-full flex items-center w-full justify-center">
+            <Card
+                className="p-6 h-16 w-80"
+                variant={"opaque"}
+            >
 
-        </div>
+
+
+            </Card>
+
+        </main >
     )
 }
