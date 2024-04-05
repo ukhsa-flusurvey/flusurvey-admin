d9f62505d94eae3532752c2eea616ad7c6693b03
phev8 <phev8@users.noreply.github.com>
2024-04-05 16:30:30 +0200

    add collapsed content for editor and preview sides

--- components/survey-editor/components/ItemEditor/editor/components/content-editor/ContentEditor.tsx
+++ components/survey-editor/components/ItemEditor/editor/components/content-editor/ContentEditor.tsx
@@ -5,7 +5,8 @@ import { ResizableHandle, ResizablePanel, ResizablePanelGroup } from '@/componen
 import ItemPreview from './ItemPreview';
 import { useDebounceCallback } from 'usehooks-ts';
 import ItemTypeEditorSelector from './ItemTypeEditorSelector';
-import SurveyLanguageToggle from '@/components/survey-editor/components/general/SurveyLanguageToggle';
+import { Eye, Info, Pencil } from 'lucide-react';
+import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from '@/components/ui/tooltip';
 
 interface ContentEditorProps {
     surveyItem: SurveyItem;
@@ -15,6 +16,8 @@ interface ContentEditorProps {
 
 const ContentEditor: React.FC<ContentEditorProps> = (props) => {
     const [currentSurveyItem, setCurrentSurveyItem] = React.useState(props.surveyItem);
+    const [editorCollapsed, setEditorCollapsed] = React.useState(false);
+    const [previewCollapsed, setPreviewCollapsed] = React.useState(false);
 
     const debouncedUpdate = useDebounceCallback(props.onUpdateSurveyItem, 700)
 
@@ -37,12 +40,35 @@ const ContentEditor: React.FC<ContentEditorProps> = (props) => {
                     collapsedSize={1}
                     defaultSize={50}
                     minSize={30}
+                    className='min-w-[56px]'
+                    onCollapse={() => {
+                        setEditorCollapsed(true);
+                    }}
+                    onExpand={() => {
+                        setEditorCollapsed(false);
+                    }}
                 >
-                    <h3 className='font-semibold text-sm'>Edit content</h3>
-                    <ItemTypeEditorSelector
-                        surveyItem={props.surveyItem}
-                        onUpdateSurveyItem={setCurrentSurveyItem}
-                    />
+                    {
+                        editorCollapsed ? <div className='flex flex-col gap-4 justify-start items-center py-6 bg-neutral-50 h-full'>
+                            <p className=''
+                                style={{
+                                    writingMode: 'vertical-rl',
+                                }}
+                            >
+                                Editor
+                            </p>
+                            <span>
+                                <Pencil className='size-4 text-neutral-500' />
+                            </span>
+                        </div> : <div className='p-4'>
+                            <h3 className='font-semibold text-base'>Edit content</h3>
+                            <ItemTypeEditorSelector
+                                surveyItem={props.surveyItem}
+                                onUpdateSurveyItem={setCurrentSurveyItem}
+                            />
+                        </div>
+                    }
+
                 </ResizablePanel>
                 <ResizableHandle
                     withHandle
@@ -52,21 +78,58 @@ const ContentEditor: React.FC<ContentEditorProps> = (props) => {
                     collapsedSize={1}
                     defaultSize={50}
                     minSize={30}
+                    className='min-w-[56px]'
+                    onCollapse={() => {
+                        setPreviewCollapsed(true);
+                    }}
+                    onExpand={() => {
+                        setPreviewCollapsed(false);
+                    }}
                 >
-                    <div className='w-full overflow-auto max-h-full'>
-                        {/**
-                             * {
-                            icon: <Eye className='me-1 size-4 text-neutral-500' />,
-                        label: 'Preview',
-        },
-                             */}
-                        <div className='h-full'>
-                            <h3 className='font-semibold text-sm'>Preview</h3>
-                            <ItemPreview
-                                surveyItem={props.surveyItem}
-                            />
+                    {
+                        previewCollapsed ? <div className='flex flex-col gap-4 justify-start items-center py-6 bg-neutral-50 h-full'>
+                            <p className=''
+                                style={{
+                                    writingMode: 'vertical-rl',
+                                }}
+                            >
+                                Preview
+                            </p>
+                            <span>
+                                <Eye className='size-4 text-neutral-500' />
+                            </span>
+                        </div> : <div className='p-4'>
+                            <TooltipProvider>
+                                <Tooltip delayDuration={500}>
+                                    <TooltipTrigger asChild>
+                                        <h3 className='font-semibold text-base flex items-center gap-2'>
+                                            <span
+                                                className='grow'
+                                            >Preview</span>
+                                            <span>
+                                                <Info className='size-4 text-neutral-500' />
+                                            </span>
+                                        </h3>
+                                    </TooltipTrigger>
+                                    <TooltipContent
+                                        className='w-96'
+                                        //side='right'
+                                        align='end'
+                                    >
+                                        <p>
+                                            Hint: The preview ignores conditions, disabling and validation rules. Expression values are replaced with a placeholder.
+                                        </p>
+                                        <p>
+                                            Use the simulator to test the item with all rules applied.
+                                        </p>
+                                    </TooltipContent>
+                                </Tooltip>
+                                <ItemPreview
+                                    surveyItem={props.surveyItem}
+                                />
+                            </TooltipProvider>
                         </div>
-                    </div>
+                    }
                 </ResizablePanel>
             </ResizablePanelGroup>
         </div>
