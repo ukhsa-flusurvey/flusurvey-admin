adcee8097a71d286a0f6221a7c682f8e9ed45251
phev8 <phev8@users.noreply.github.com>
2024-04-15 12:43:54 +0200

    improve component navigation

--- components/survey-editor/components/ItemEditor/editor/components/content-editor/ItemTypeEditorSelector.tsx
+++ components/survey-editor/components/ItemEditor/editor/components/content-editor/ItemTypeEditorSelector.tsx
@@ -2,19 +2,9 @@ import { getItemTypeInfos } from '@/components/survey-editor/utils/utils';
 import React from 'react';
 import { SurveyItem } from 'survey-engine/data_types';
 import SurveyEndEditor from './SurveyEndEditor';
-import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from '@/components/ui/accordion';
-import { Input } from '@/components/ui/input';
-import { CircleHelp, Heading1, Heading2, MessageSquareReply, PanelBottom, PanelTop, Subscript } from 'lucide-react';
-import { Collapsible, CollapsibleContent, CollapsibleTrigger } from '@/components/ui/collapsible';
-import SurveyLanguageToggle from '@/components/survey-editor/components/general/SurveyLanguageToggle';
 import '@mdxeditor/editor/style.css'
-import HelpPopupEditor from './help-popup-editor';
-import SubtitleEditor from './subtitle-editor';
-import TitleEditor from './title-editor';
 import TopContentEditor from './top-content-editor';
-import FootnoteEditor from './footnote-editor';
-import BottomContentEditor from './bottom-content-editor';
-import ResponseGroupEditor from './response-editors/response-group-editor';
+import ComponentSelector from './component-selector';
 
 interface ItemTypeEditorSelectorProps {
     surveyItem: SurveyItem;
@@ -48,145 +38,11 @@ const ItemTypeEditorSelector: React.FC<ItemTypeEditorSelectorProps> = (props) =>
     return (
         <div>
 
-            <div className='mx-auto py-6'>
-
-                <Accordion type="single" collapsible>
-
-                    <div className='mb-6 p-4 rounded-md border border-l-4  border-l-[--survey-card-header-bg]'>
-
-                        <h4 className='text-xs tracking-widest font-semibold'>
-                            Header
-                        </h4>
-                        <AccordionItem value="title">
-                            <AccordionTrigger>
-                                <span className='flex items-center text-sm'>
-                                    <span>
-                                        <Heading1 className='size-4 mr-2 text-neutral-600' />
-                                    </span>
-                                    Title
-                                </span>
-                            </AccordionTrigger>
-                            <AccordionContent>
-                                <TitleEditor
-                                    surveyItem={props.surveyItem}
-                                    onUpdateSurveyItem={props.onUpdateSurveyItem}
-                                />
-                            </AccordionContent>
-                        </AccordionItem>
-
-                        <AccordionItem value="subtitle">
-                            <AccordionTrigger>
-                                <span className='flex items-center text-sm text-muted-foreground'>
-                                    <span>
-                                        <Heading2 className='size-4 mr-2 text-neutral-600' />
-                                    </span>
-                                    Subtitle
-                                </span>
-                            </AccordionTrigger>
-                            <AccordionContent>
-                                <SubtitleEditor
-                                    surveyItem={props.surveyItem}
-                                    onUpdateSurveyItem={props.onUpdateSurveyItem}
-                                />
-                            </AccordionContent>
-                        </AccordionItem>
-
-                        <AccordionItem value="help-popup">
-                            <AccordionTrigger>
-                                <span className='flex items-center text-sm text-muted-foreground'>
-                                    <span>
-                                        <CircleHelp className='size-4 mr-2 text-neutral-600' />
-                                    </span>
-                                    Help/Info popup
-                                </span>
-                            </AccordionTrigger>
-                            <AccordionContent>
-                                <HelpPopupEditor
-                                    surveyItem={props.surveyItem}
-                                    onUpdateSurveyItem={props.onUpdateSurveyItem}
-                                />
-                            </AccordionContent>
-                        </AccordionItem>
-                    </div>
-
-
-                    <div className='mb-6 p-4 rounded-md border border-l-4  border-[--survey-card-bg]'>
-
-                        <h4 className='text-xs tracking-widest font-semibold'>
-                            Body
-                        </h4>
-                        <AccordionItem value="top-contents">
-                            <AccordionTrigger>
-                                <span className='flex items-center text-sm text-muted-foreground'>
-                                    <span>
-                                        <PanelTop className='size-4 mr-2 text-neutral-600' />
-                                    </span>
-                                    Top content (before response)
-                                </span>
-                            </AccordionTrigger>
-                            <AccordionContent>
-                                <TopContentEditor
-                                    surveyItem={props.surveyItem}
-                                    onUpdateSurveyItem={props.onUpdateSurveyItem}
-                                />
-                            </AccordionContent>
-                        </AccordionItem>
-
-                        <AccordionItem value="response">
-                            <AccordionTrigger>
-                                <span className='flex items-center text-sm'>
-                                    <span>
-                                        <MessageSquareReply className='size-4 mr-2 text-neutral-600' />
-                                    </span>
-                                    Response options
-                                </span>
-                            </AccordionTrigger>
-                            <AccordionContent>
-                                <ResponseGroupEditor
-                                    surveyItem={props.surveyItem}
-                                    onUpdateSurveyItem={props.onUpdateSurveyItem}
-                                />
-                            </AccordionContent>
-                        </AccordionItem>
-
-                        <AccordionItem value="bottom-contents">
-                            <AccordionTrigger>
-                                <span className='flex items-center text-sm text-muted-foreground'>
-                                    <span>
-                                        <PanelBottom className='size-4 mr-2 text-neutral-600' />
-                                    </span>
-                                    Bottom content (after response)
-                                </span>
-                            </AccordionTrigger>
-                            <AccordionContent>
-                                <BottomContentEditor
-                                    surveyItem={props.surveyItem}
-                                    onUpdateSurveyItem={props.onUpdateSurveyItem}
-                                />
-                            </AccordionContent>
-                        </AccordionItem>
-                    </div>
-
-                    <div className='ps-4 border-l-4 border-neutral-100'>
-                        <AccordionItem value="footnote">
-                            <AccordionTrigger>
-                                <span className='flex items-center text-sm text-muted-foreground'>
-                                    <span>
-                                        <Subscript className='size-4 mr-2 text-neutral-600' />
-                                    </span>
-                                    Footnote
-                                </span>
-                            </AccordionTrigger>
-                            <AccordionContent>
-                                <FootnoteEditor
-                                    surveyItem={props.surveyItem}
-                                    onUpdateSurveyItem={props.onUpdateSurveyItem}
-                                />
-                            </AccordionContent>
-                        </AccordionItem>
-                    </div>
-                </Accordion>
-
+            <div className='mx-auto mt-4'>
+                <ComponentSelector
+                    surveyItem={props.surveyItem}
+                    onUpdateSurveyItem={props.onUpdateSurveyItem}
+                />
             </div>
         </div >
     );
---
+++ components/survey-editor/components/ItemEditor/editor/components/content-editor/component-selector.tsx
@@ -0,0 +1,222 @@
+import { Button } from '@/components/ui/button';
+import { Separator } from '@/components/ui/separator';
+import { ArrowLeft, ChevronRight, CircleHelp, Heading1, Heading2, MessageSquareReply, PanelBottom, PanelTop, Subscript } from 'lucide-react';
+import React from 'react';
+import TitleEditor from './title-editor';
+import { SurveyItem } from 'survey-engine/data_types';
+import SubtitleEditor from './subtitle-editor';
+import HelpPopupEditor from './help-popup-editor';
+import TopContentEditor from './top-content-editor';
+import ResponseGroupEditor from './response-editors/response-group-editor';
+import BottomContentEditor from './bottom-content-editor';
+import FootnoteEditor from './footnote-editor';
+
+
+interface ComponentSelectorProps {
+    surveyItem: SurveyItem;
+    onUpdateSurveyItem: (item: SurveyItem) => void;
+}
+
+const headerComponents = [
+    {
+        key: 'title',
+        label: 'Title',
+        icon: <Heading1 className='size-4 text-muted-foreground-600' />,
+    },
+    {
+        key: 'subtitle',
+        label: 'Subtitle',
+        icon: <Heading2 className='size-4 text-muted-foreground-600' />,
+    },
+    {
+        key: 'help-popup',
+        label: 'Help/Info popup',
+        icon: <CircleHelp className='size-4 text-muted-foreground-600' />,
+    },
+];
+
+const bodyComponents = [
+    {
+        key: 'top-contents',
+        label: 'Top content (before response)',
+        icon: <PanelTop className='size-4 text-muted-foreground-600' />,
+    },
+    {
+        key: 'response',
+        label: 'Response options',
+        icon: <MessageSquareReply className='size-4 text-muted-foreground-600' />,
+    },
+    {
+        key: 'bottom-contents',
+        label: 'Bottom content (after response)',
+        icon: <PanelBottom className='size-4 text-muted-foreground-600' />,
+    },
+];
+
+
+const ComponentSelector: React.FC<ComponentSelectorProps> = (props) => {
+    const [selectedComponent, setSelectedComponent] = React.useState<string | undefined>(undefined);
+
+    const renderHeader = () => {
+        if (!selectedComponent) {
+            return null;
+        }
+
+        return (<div className=''>
+            <Button className='text-xs px-1' variant={'ghost'}
+                onClick={() => setSelectedComponent(undefined)}
+            >
+                <span><ArrowLeft className='size-4 me-2'></ArrowLeft></span>
+                Back to components
+            </Button>
+            <Separator />
+            <h4 className='font-semibold mt-2 mb-4'>
+                {selectedComponent.toUpperCase()}
+            </h4>
+        </div>
+        );
+    }
+
+
+
+    switch (selectedComponent) {
+        case 'title':
+            return (
+                <div className=''>
+                    {renderHeader()}
+                    <TitleEditor
+                        surveyItem={props.surveyItem}
+                        onUpdateSurveyItem={props.onUpdateSurveyItem}
+                    />
+                </div>
+            );
+        case 'subtitle':
+            return (
+                <div className=''>
+                    {renderHeader()}
+                    <SubtitleEditor
+                        surveyItem={props.surveyItem}
+                        onUpdateSurveyItem={props.onUpdateSurveyItem}
+                    />
+                </div>
+            );
+        case 'help-popup':
+            return (
+                <div className=''>
+                    {renderHeader()}
+                    <HelpPopupEditor
+                        surveyItem={props.surveyItem}
+                        onUpdateSurveyItem={props.onUpdateSurveyItem}
+                    />
+                </div>
+            );
+        case 'top-contents':
+            return (
+                <div className=''>
+                    {renderHeader()}
+                    <TopContentEditor
+                        surveyItem={props.surveyItem}
+                        onUpdateSurveyItem={props.onUpdateSurveyItem}
+                    />
+                </div>
+            );
+        case 'response':
+            return (
+                <div>
+                    {renderHeader()}
+                    <ResponseGroupEditor
+                        surveyItem={props.surveyItem}
+                        onUpdateSurveyItem={props.onUpdateSurveyItem}
+                    />
+                </div>
+            )
+        case 'bottom-contents':
+            return (
+                <div>
+                    {renderHeader()}
+                    <BottomContentEditor
+                        surveyItem={props.surveyItem}
+                        onUpdateSurveyItem={props.onUpdateSurveyItem}
+                    />
+                </div>
+            )
+        case 'footnote':
+            return (
+                <div>
+                    {renderHeader()}
+                    <FootnoteEditor
+                        surveyItem={props.surveyItem}
+                        onUpdateSurveyItem={props.onUpdateSurveyItem}
+                    />
+                </div>
+            );
+        default:
+            return (
+                <div>
+                    <div className='mb-6 p-4 rounded-md border border-l-4  border-l-[--survey-card-header-bg]'>
+
+                        <h4 className='text-xs tracking-widest font-semibold'>
+                            Header
+                        </h4>
+                        <ul className='divide-y'>
+                            {headerComponents.map((comp, index) => (
+                                <li className='py-1' key={comp.key + index.toFixed()}>
+                                    <Button
+                                        className='w-full flex justify-start gap-2 items-center'
+                                        variant={'ghost'}
+                                        onClick={() => setSelectedComponent(comp.key)}
+                                    >
+                                        {comp.icon}
+                                        <span className='grow text-start'>{comp.label}</span>
+                                        <span><ChevronRight
+                                            className='size-4 text-muted-foreground'
+                                        ></ChevronRight></span>
+                                    </Button>
+                                </li>
+                            ))}
+                        </ul>
+                    </div>
+
+                    <div className='mb-6 p-2 rounded-md border border-l-4  border-[--survey-card-bg]'>
+                        <h4 className='text-xs tracking-widest font-semibold'>
+                            Body
+                        </h4>
+                        <ul className='divide-y'>
+                            {bodyComponents.map((comp, index) => (
+                                <li className='py-1' key={comp.key + index.toFixed()}>
+                                    <Button
+                                        className='w-full flex justify-start gap-2 items-center'
+                                        variant={'ghost'}
+                                        onClick={() => setSelectedComponent(comp.key)}
+                                    >
+                                        {comp.icon}
+                                        <span className='grow text-start'>{comp.label}</span>
+                                        <span><ChevronRight
+                                            className='size-4 text-muted-foreground'
+                                        ></ChevronRight></span>
+                                    </Button>
+                                </li>
+                            ))}
+                        </ul>
+                    </div>
+
+                    <div className='p-2 border-l-4 rounded-md border border-neutral-100'>
+                        <Button
+                            className='w-full flex justify-start gap-2 items-center'
+                            variant={'ghost'}
+                            onClick={() => setSelectedComponent('footnote')}
+                        >
+                            <Subscript className='size-4 text-muted-foreground' />
+                            <span className='grow text-start'>Footnote</span>
+                            <span><ChevronRight
+                                className='size-4 text-muted-foreground'
+                            ></ChevronRight></span>
+                        </Button>
+                    </div>
+                </div>
+            );
+    }
+
+};
+
+export default ComponentSelector;
