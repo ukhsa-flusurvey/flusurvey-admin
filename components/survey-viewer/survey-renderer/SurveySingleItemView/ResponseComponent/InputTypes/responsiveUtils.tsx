85cfd095abb7fe012b172712a54d84577d42dc4c
phev8 <phev8@users.noreply.github.com>
2024-05-19 13:48:39 +0200

    implement responsive single choice array improved component

--- components/survey-viewer/survey-renderer/SurveySingleItemView/ResponseComponent/InputTypes/responsiveUtils.tsx
+++ components/survey-viewer/survey-renderer/SurveySingleItemView/ResponseComponent/InputTypes/responsiveUtils.tsx
@@ -1,13 +1,13 @@
-import clsx from "clsx";
 import React from "react";
 import { getClassName } from "../../utils";
+import { cn } from "@/lib/utils";
 
 const bootstrapBreakpoints = ['sm', 'md', 'lg', 'xl', 'xxl'];
 
 interface BreakpointModeDef {
-  start?: string;
-  end?: string;
-  variant: Variant;
+    start?: string;
+    end?: string;
+    variant: Variant;
 }
 
 export type Variant = BipolarLikertScaleVariant | SingleChoiceVariant;
@@ -16,67 +16,80 @@ type SingleChoiceVariant = 'vertical' | 'horizontal' | 'table';
 type BipolarLikertScaleVariant = 'vertical' | 'withLabelRow' | 'table';
 
 const getModeForBreakpont = (bp: string, styles?: Array<{ key: string, value: string }>): Variant | undefined => {
-  if (!styles) { return undefined };
-  const bpMode = styles.find(style => style.key === `${bp}Mode`);
-  if (!bpMode) {
-    return undefined;
-  }
-  return bpMode.value as Variant;
+    if (!styles) { return undefined };
+    const bpMode = styles.find(style => style.key === `${bp}Mode`);
+    if (!bpMode) {
+        return undefined;
+    }
+    return bpMode.value as Variant;
 }
 
 const getDefaultMode = (styles?: Array<{ key: string, value: string }>): Variant | undefined => {
-  if (!styles) { return undefined };
-  const bpMode = styles.find(style => style.key === 'defaultMode');
-  if (!bpMode) {
-    return 'vertical';
-  }
-  return bpMode.value as Variant;
+    if (!styles) { return undefined };
+    const bpMode = styles.find(style => style.key === 'defaultMode');
+    if (!bpMode) {
+        return 'vertical';
+    }
+    return bpMode.value as Variant;
 }
 
-export const getResponsiveModes = (renderMode: (variant: Variant, namePrefix: string) => React.ReactNode, styles?: Array<{ key: string, value: string }>) => {
-  const breakpointModes: Array<BreakpointModeDef> = [];
-  const defaultMode = getDefaultMode(styles);
-  if (!defaultMode) {
-    return <p>No default mode found.</p>
-  }
-  let currentMode: BreakpointModeDef = {
-    variant: defaultMode
-  };
-
-  bootstrapBreakpoints.forEach(bp => {
-    const mode = getModeForBreakpont(bp, styles);
-    if (mode === undefined) {
-      return;
+export const getResponsiveModes = (width: number, renderMode: (variant: Variant, namePrefix: string) => React.ReactNode, styles?: Array<{ key: string, value: string }>) => {
+    const defaultMode = getDefaultMode(styles);
+    if (!defaultMode) {
+        return <p>No default mode found.</p>
     }
-    currentMode.end = bp;
-    breakpointModes.push(currentMode);
-    currentMode = {
-      start: bp,
-      variant: mode,
+    let currentMode: BreakpointModeDef = {
+        variant: defaultMode
+    };
+
+    const screenSize = getScreenSizeLabel(width);
+    const screenSizeIndex = bootstrapBreakpoints.indexOf(screenSize);
+
+    bootstrapBreakpoints.forEach((bp, index) => {
+        if (index >= screenSizeIndex) {
+            return
+        }
+        const mode = getModeForBreakpont(bp, styles);
+        if (mode === undefined) {
+            return;
+        }
+        currentMode.end = bp;
+        currentMode = {
+            start: bp,
+            variant: mode,
+        }
+    });
+
+
+    const className = getClassName(styles);
+
+    return (
+        <React.Fragment>
+            <div
+                className={cn(
+                    'overflow-visible',
+                    className,
+                )}
+            >
+                {renderMode(currentMode.variant, currentMode.start ? currentMode.start : 'default')}
+            </div>
+        </React.Fragment>
+    )
+}
+
+
+const getScreenSizeLabel = (width: number): string => {
+    if (width < 649) {
+        return 'xs';
     }
-  });
-  breakpointModes.push(currentMode);
-
-  const className = getClassName(styles);
-
-  return (
-    <React.Fragment>
-      {
-        breakpointModes.map((bm, index) => <div
-          key={index.toString()}
-          className={clsx(
-            'overflow-visible',
-            {
-              'd-none': bm.start !== undefined,
-              [`d-${bm.start}-block`]: bm.start !== undefined,
-              [`d-${bm.end}-none`]: bm.end !== undefined
-            },
-            className,
-          )}
-        >
-          {renderMode(bm.variant, bm.start ? bm.start : 'default')}
-        </div>)
-      }
-    </React.Fragment>
-  )
+    if (width < 768) {
+        return 'sm';
+    }
+    if (width < 1024) {
+        return 'md';
+    }
+    if (width < 1280) {
+        return 'lg';
+    }
+    return 'xl';
 }
