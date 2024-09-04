588428a9d16d31ed5b72029b049c506602f7d160
Sam Morris <sam.morris@ukhsa.gov.uk>
2024-09-04 10:11:20 +0100

    Add content-security-policy and permissions-policy headers

--- next.config.js
+++ next.config.js
@@ -5,6 +5,23 @@ const nextConfig = {
         serverActions: {
             bodySizeLimit: '10mb'
         }
+    },
+    async headers() {
+        return [
+            {
+                source: "^",
+                headers: [
+                    {
+                       key: "Content-Security-Policy",
+                       value: process.env.CSP_HEADER_VALUE || "default-src 'self'; frame-ancestors 'none'; style-src 'self' 'unsafe-inline'; script-src 'self' 'unsafe-eval' 'unsafe-inline'; connect-src 'self'; img-src 'self' data:",
+                    },
+                    {
+                      key: "Permissions-Policy",
+                      value: process.env.PERMISSIONS_POLICY_HEADER_VALUE || "camera=(), microphone=(), geolocation=()",
+                    }
+                ]
+            }
+	]
     }
 }
 
