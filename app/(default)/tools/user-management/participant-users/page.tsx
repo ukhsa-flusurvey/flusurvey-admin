7e234c6892e268d6c653002667ed6776dbcb130d
phev8 <phev8@users.noreply.github.com>
2024-09-13 16:57:08 +0200

    add page for participant-user management

---
+++ app/(default)/tools/user-management/participant-users/page.tsx
@@ -0,0 +1,15 @@
+import TabNav from "../_components/tab-nav";
+
+export default function Page() {
+
+    return (
+        <div className="space-y-6">
+            <TabNav
+                activeTab={'participant-users'}
+            />
+            <div className="flex w-full">
+                <h1>TODO page for: app/(default)/tools/user-management/participant-users/page.tsx</h1>
+            </div>
+        </div>
+    );
+}
