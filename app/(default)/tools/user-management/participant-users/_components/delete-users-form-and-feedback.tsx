911bb7f5a73cb8a25297d0f8b1e3673853b75028
phev8 <phev8@users.noreply.github.com>
2024-09-19 13:02:54 +0200

    clean up import

--- app/(default)/tools/user-management/participant-users/_components/delete-users-form-and-feedback.tsx
+++ app/(default)/tools/user-management/participant-users/_components/delete-users-form-and-feedback.tsx
@@ -7,7 +7,7 @@ import { Label } from '@/components/ui/label';
 import { Textarea } from '@/components/ui/textarea';
 import { cn } from '@/lib/utils';
 import { Loader2 } from 'lucide-react';
-import React, { use, useEffect } from 'react';
+import React, { useEffect } from 'react';
 
 interface DeleteUsersFormAndFeedbackProps {
 }
