09ecade6a979b386b0fe9c98c85104e0c5013719
phev8 <phev8@users.noreply.github.com>
2025-02-20 15:06:52 +0100

    add page and nav item

---
+++ app/(default)/tools/participants/[studyKey]/actions/participant-info-updater/page.tsx
@@ -0,0 +1,8 @@
+export default async function Page() {
+
+    return (
+        <div>
+            <h1>TODO page for: app/(default)/tools/participants/[studyKey]/actions/participant-info-updater/page.tsx</h1>
+        </div>
+    );
+}
