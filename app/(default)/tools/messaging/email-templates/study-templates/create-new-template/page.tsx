97ec080a146025e490a4d54af8e3a51f2d9f6f9b
phev8 <phev8@users.noreply.github.com>
2025-04-29 17:09:39 +0200

    Add availableStudyKeys prop to EmailTemplateConfigurator and MessageConfig components

--- app/(default)/tools/messaging/email-templates/study-templates/create-new-template/page.tsx
+++ app/(default)/tools/messaging/email-templates/study-templates/create-new-template/page.tsx
@@ -1,9 +1,14 @@
+import { getStudies } from "@/lib/data/studyAPI";
 import EmailTemplateConfigurator from "../../_components/EmailTemplateConfigurator";
 import SimpleBreadcrumbsPageLayout from "@/components/SimpleBreadcrumbsPageLayout";
 
 export const dynamic = 'force-dynamic';
 
-export default function Page() {
+export default async function Page() {
+
+    const studies = await getStudies();
+    const availableStudyKeys = studies.studies?.map(s => s.key);
+    console.log(availableStudyKeys)
 
     return (
         <SimpleBreadcrumbsPageLayout
@@ -31,6 +36,7 @@ export default function Page() {
                         messageType={`new-message-${(Math.random() * 100).toFixed(0)}`}
                         isSystemTemplate={false}
                         isGlobalTemplate={false}
+                        availableStudyKeys={availableStudyKeys}
                     />
                 </div>
             </div>
