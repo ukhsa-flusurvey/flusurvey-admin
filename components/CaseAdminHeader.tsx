7acb6b9b3922fd01e27608a85006161bd34f277c
phev8 <phev8@users.noreply.github.com>
2025-08-18 11:18:32 +0200

    Refactor CaseAdminHeader to remove appName prop and use environment variable directly. Update page components to reflect this change and include NAME_SUFFIX in the display.

--- components/CaseAdminHeader.tsx
+++ components/CaseAdminHeader.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Link from 'next/link';
 
-interface CaseAdminHeaderProps {
-    appName: string;
-}
+export const dynamic = 'force-dynamic';
+export const revalidate = false;
 
-const CaseAdminHeader: React.FC<CaseAdminHeaderProps> = (props) => {
+
+const CaseAdminHeader: React.FC = async () => {
+    const appName = process.env.NEXT_PUBLIC_APP_NAME || 'Editor Tools';
+
     return (
         <Link
             prefetch={false}
@@ -13,7 +15,7 @@ const CaseAdminHeader: React.FC<CaseAdminHeaderProps> = (props) => {
             className='flex border-l-[5px] border-l-cyan-800 px-4'>
             <h1 className='text-2xl'>
                 <span className='font-normal text-cyan-800 text-lg'>CASE ADMIN</span> <br />
-                <span className='font-semibold tracking-wider'>{props.appName}</span>
+                <span className='font-semibold tracking-wider'>{appName} {process.env.NAME_SUFFIX}</span>
             </h1>
         </Link>
     );
