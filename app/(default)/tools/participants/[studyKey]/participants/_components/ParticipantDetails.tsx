d6bce54b9bb165c2714624df14e7414c0e354bae
phev8 <phev8@users.noreply.github.com>
2025-10-20 12:47:39 +0200

    feat: add linking codes management to ParticipantDetails component
    - Introduced a new LinkingCodeSection for displaying, editing, and adding linking codes for participants.
    - Enhanced UI with a table layout for linking codes, including options to edit and delete codes.
    - Integrated dialogs for editing and adding new linking codes, improving user interaction and data management.
    - Updated ParticipantDetails component to include the new LinkingCodeSection, ensuring seamless participant updates.

--- app/(default)/tools/participants/[studyKey]/participants/_components/ParticipantDetails.tsx
+++ app/(default)/tools/participants/[studyKey]/participants/_components/ParticipantDetails.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import AvatarFromId from '@/components/AvatarFromID';
-import { Activity, Calendar, Edit2, FlagTriangleRight, MoreVertical, PencilIcon, Plus, Tag, Trash2 } from 'lucide-react';
+import { Activity, Calendar, Edit2, FlagTriangleRight, Link, MoreVertical, PencilIcon, Plus, Tag, Trash2 } from 'lucide-react';
 import React, { useState } from 'react';
 import CopyIdToClipboad from './CopyIdToClipboad';
 import { Separator } from '@/components/ui/separator';
@@ -326,6 +326,148 @@ const ParticipantFlagSection = (props: {
     )
 }
 
+const LinkingCodeSection = (props: {
+    participant?: ParticipantState;
+    isLoading: boolean;
+    onChange: (participant: ParticipantState) => void;
+}) => {
+    const participant = props.participant;
+    const [linkingCodeToEdit, setLinkingCodeToEdit] = useState<{ key: string, value: string } | undefined>(undefined);
+    const [newLinkingCodeEditorOpen, setNewLinkingCodeEditorOpen] = useState(false);
+
+    if (!participant) return null;
+
+    const linkingCodes = participant?.linkingCodes ? Object.entries(participant.linkingCodes) : [];
+
+    return (
+        <div>
+            <div className='flex items-center gap-2 justify-between'>
+                <h3 className='text-sm font-bold flex items-center gap-2'>
+                    <span className='text-muted-foreground'>
+                        <Link className='size-3' />
+                    </span>
+                    Linking codes
+                </h3>
+
+                <Button
+                    variant='ghost'
+                    size='icon'
+                    onClick={() => setNewLinkingCodeEditorOpen(true)}
+                >
+                    <Plus className='size-4' />
+                </Button>
+            </div>
+
+            <div className='rounded-md border border-border overflow-hidden'>
+                {linkingCodes.length > 0 && <Table className='text-xs'>
+                    <TableHeader className='bg-muted'>
+                        <TableRow>
+                            <TableHead className='h-auto p-2'>KEY</TableHead>
+                            <TableHead className='h-auto p-2'>VALUE</TableHead>
+                            <TableHead className='h-auto w-16'>
+                                <span className='sr-only'>Actions</span>
+                            </TableHead>
+                        </TableRow>
+                    </TableHeader>
+                    <TableBody>
+                        {linkingCodes.map(([key, value]) => (
+                            <TableRow key={key} className='font-mono'>
+                                <TableCell className='p-2'>{key}</TableCell>
+                                <TableCell className='p-2'>{value}</TableCell>
+                                <TableCell className='p-0'>
+                                    <DropdownMenu>
+                                        <DropdownMenuTrigger asChild>
+                                            <Button variant='ghost' className='w-full h-8'>
+                                                <span className='sr-only'>Open menu</span>
+                                                <MoreVertical className='h-4 w-4' />
+                                            </Button>
+                                        </DropdownMenuTrigger>
+                                        <DropdownMenuContent align='end'>
+                                            <DropdownMenuItem onClick={() => {
+                                                setLinkingCodeToEdit({ key: key, value: value });
+                                            }}>
+                                                <Edit2 className='mr-2 h-4 w-4' />
+                                                Edit
+                                            </DropdownMenuItem>
+                                            <DropdownMenuSeparator />
+                                            <DropdownMenuItem onClick={() => {
+                                                if (confirm('Are you sure you want to delete this flag?')) {
+                                                    const newLinkingCodes = { ...participant?.linkingCodes };
+                                                    delete newLinkingCodes[key];
+                                                    props.onChange({ ...participant, linkingCodes: newLinkingCodes });
+                                                }
+                                            }}>
+                                                <Trash2 className='mr-2 h-4 w-4' />
+                                                Delete
+                                            </DropdownMenuItem>
+                                        </DropdownMenuContent>
+                                    </DropdownMenu>
+                                </TableCell>
+                            </TableRow>
+                        ))}
+                    </TableBody>
+                </Table>
+                }
+
+                {linkingCodes.length === 0 && <div className='flex items-center justify-center h-16 text-xs text-muted-foreground'>
+                    No linking codes
+                </div>}
+            </div>
+
+            <Dialog
+                open={linkingCodeToEdit !== undefined}
+                onOpenChange={(open) => {
+                    if (!open) {
+                        setLinkingCodeToEdit(undefined);
+                    }
+                }}
+            >
+                <DialogContent>
+                    <DialogHeader>
+                        <DialogTitle>
+                            Edit linking code
+                        </DialogTitle>
+                    </DialogHeader>
+                    <FlagEditorForm
+                        initialValue={linkingCodeToEdit}
+                        usedKeys={linkingCodes.map(([key,]) => key)}
+                        onSave={(key, value) => {
+                            props.onChange({ ...participant, linkingCodes: { ...participant?.linkingCodes, [key]: value } });
+                            setLinkingCodeToEdit(undefined);
+                        }}
+                        onCancel={() => {
+                            setLinkingCodeToEdit(undefined);
+                        }}
+                    />
+                </DialogContent>
+            </Dialog>
+
+            <Dialog
+                open={newLinkingCodeEditorOpen}
+                onOpenChange={setNewLinkingCodeEditorOpen}
+            >
+                <DialogContent>
+                    <DialogHeader>
+                        <DialogTitle>
+                            Add new linking code
+                        </DialogTitle>
+                    </DialogHeader>
+                    <FlagEditorForm
+                        usedKeys={linkingCodes.map(([key,]) => key)}
+                        onSave={(key, value) => {
+                            props.onChange({ ...participant, linkingCodes: { ...participant?.linkingCodes, [key]: value } });
+                            setNewLinkingCodeEditorOpen(false);
+                        }}
+                        onCancel={() => {
+                            setNewLinkingCodeEditorOpen(false);
+                        }}
+                    />
+                </DialogContent>
+            </Dialog>
+        </div>
+    )
+}
+
 const ParticipantDetails: React.FC<ParticipantDetailsProps> = (props) => {
     const participant = props.participant;
     const lastModified = participant?.modifiedAt ? participant.modifiedAt : participant?.lastSubmissions ? Object.values(participant.lastSubmissions).sort((a, b) => b - a)[0] : undefined;
@@ -452,6 +594,14 @@ const ParticipantDetails: React.FC<ParticipantDetailsProps> = (props) => {
                 <Separator />
 
                 <ParticipantFlagSection
+                    participant={participant}
+                    isLoading={isLoading}
+                    onChange={(participant) => {
+                        onUpdateParticipant(participant);
+                    }}
+                />
+
+                <LinkingCodeSection
                     participant={participant}
                     isLoading={isLoading}
                     onChange={(participant) => {
